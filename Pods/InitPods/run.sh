49631b67f303a966782963348c624745193b7ef0
chaitu1 <chaitu143@gmail.com>
2024-05-18 14:29:16 +0530

    config map is completed

--- Pods/InitPods/run.sh
+++ Pods/InitPods/run.sh
@@ -1,5 +1,19 @@
 IFS=","
+#Message=`echo Message Parameter Not Found: $i && exit 1`
 for i in $PARAMS
-do
-        aws ssm get-parameters --name $i --region ap-south-1
+        do
+                value=`aws ssm get-parameter --name $i --region ap-south-1 --query  "Parameter.Value" --output text`
+
+                [ -z $value ] && echo -e "\e[31mVARIABLE IS MISSING :$i \e[0m" && exit 1 || echo "$i=$value" >> /params
+               #aws ssm get-parameters --name $i --region ap-south-1 --query  "Parameters[].Value" --output text
 done
+
+while true
+do
+        echo -e "------------PARAMAS----------"
+        cat /params
+        echo -e "------------PARAMAS-----------\n\n"
+        sleep 2
+done
+
+
