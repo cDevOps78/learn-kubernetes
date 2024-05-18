f9072e9d64f9afcf32135b9dba303893d6fe6976
chaitu1 <chaitu143@gmail.com>
2024-05-18 14:53:59 +0530

    config map is completed

--- Pods/InitPods/run1.sh
+++ Pods/InitPods/run1.sh
@@ -1,10 +1,19 @@
 
-# PARAMS="app_name,APP_NAME app_project_name,APP_PROJECT_NAME app_version,APP_VERSION"
+# PARAMS="app_name,APP_NAME app_project_name,APP_PROJECT_NAME app_versivi
+
+
+
+
 for i in $PARAMS
 do
         key=$(echo $i | awk '{print $1}' | awk -F"," '{print $1}')
         value=$(echo $i | awk '{print $1}' | awk -F"," '{print $2}')
         value=`aws ssm get-parameter --name $value --region ap-south-1 --query  "Parameter.Value" --output text`
 
-        echo "$key:$value"
+        echo "export $key:$value" >> /tmp/params
 done
+
+
+cat /tmp/params
+
+> /tmp/params
