9dbc6967798e93952fd75aa95522b0b5f7fbca1a
chaitu1 <chaitu143@gmail.com>
2024-05-18 09:26:51 +0530

    Learning terraform functions

--- Pods/configmapPods/run.sh
+++ Pods/configmapPods/run.sh
@@ -1,7 +1,7 @@
 for i in P-name P-app P-version P-compo
 do
   var="\$$i"
-  [ -z `eval echo $var` ] && echo "Variable is Missing: $var" && exit 1   # eval echo $Pname
+  [ -z `eval echo $var` ] && echo "Variable is Missing: $var" && exit 1   # eval echo $P-name
 done
 
 sleep 1000
