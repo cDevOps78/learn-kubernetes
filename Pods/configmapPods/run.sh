68f9c4b31d425f57c40998945fb4ec7ee044d4df
chaitu1 <chaitu143@gmail.com>
2024-05-18 15:18:41 +0530

    config map is completed

--- Pods/configmapPods/run.sh
+++ Pods/configmapPods/run.sh
@@ -1,7 +1,7 @@
 for i in Pname Papp Pversion Pcompo
 do
   var="\$$i"
-  [ -z `eval echo $var` ] && echo "Variable is Missing: $var" && exit 1   # eval echo $P-name
+  [ -z `eval echo $var` ] && echo "Variable is Missing: $var" && exit 1   # eval echo $Pname
 done
 
 while true
