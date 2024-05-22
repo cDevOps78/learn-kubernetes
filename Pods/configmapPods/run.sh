3860b966a36a34b33f9a1268e32088339ce7e13f
chaitu1 <chaitu143@gmail.com>
2024-05-22 17:28:55 +0530

    working with init containers

--- Pods/configmapPods/run.sh
+++ Pods/configmapPods/run.sh
@@ -4,8 +4,10 @@ do
   [ -z `eval echo $var` ] && echo "Variable is Missing: $var" && exit 1   # eval echo $Pname
 done
 
+# I want to run the container continuously, so with while loop.
+
 while true
 do
   echo "Sleeping chaitu"
-  sleep 2
+  sleep 3
 done
