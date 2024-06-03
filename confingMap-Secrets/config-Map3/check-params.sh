368393697be23ab5d59d7069efb6dba77917684c
chaitu1 <chaitu143@gmail.com>
2024-06-03 20:16:15 +0530

    kubernetes expense frontend is completed

---
+++ confingMap-Secrets/config-Map3/check-params.sh
@@ -0,0 +1,14 @@
+for i in $PARAMS
+do
+  var=\$${i}
+  [ -z `eval echo $var` ] && echo "Parameter is missing: $i" && exit 1 || echo "$i = $(eval echo $var)"
+done
+
+# Assume this is actual application
+while true
+do
+  echo "All variables is provided"
+  echo "Sleeping 4s"
+  sleep 4s
+done
+
